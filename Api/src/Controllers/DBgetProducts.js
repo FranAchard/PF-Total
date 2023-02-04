6dcd0a016f37b82d58ba311c94a43ba9c1296902
marsavil <msvillaescusa@gmail.com>
2023-02-04 17:21:07 -0300

    get product by query

--- Api/src/Controllers/DBgetProducts.js
+++ Api/src/Controllers/DBgetProducts.js
@@ -1,10 +1,14 @@
-const product = require("../models/product.js");
-
 const getProductDB = async(req,res)=>{
+    const {query} = req.query
     try {
         const products = await product.findAll();
-        
-        res.status(200).send(products)
+        if(query){
+            const search = products.filter((p)=>p.model.toUpperCase().includes(query.toUpperCase()))
+                
+            res.status(200).send(search)
+        }else{
+            res.status(200).send(products)
+        }       
     } catch (error) {
         res.status(400).send({error: error.message})
     }
