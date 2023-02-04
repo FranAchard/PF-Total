c53f34a37e78bff59f16254f75044a5e92d1a378
Mariano Villaescusa <84275466+marsavil@users.noreply.github.com>
2023-02-04 11:19:16 +0100

    Update Landing.jsx
    Link estaba definido dos vences.
    flataba el parentesis de cierre en la linea 33
    y por simple histeria agregue las tabulaciones correctas.

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -2,34 +2,34 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import "./landing.css"
-import {Link} from "react-router-dom"
 import video from "../../Media/MÃ³vil - 29326.mp4"
 
 function Landing() {
     return (
         <header>
-        <div class="container">
-         <div class="overlay">
+            <div class="container">
+                <div class="overlay">
  
-         <video autoplay="autoplay" muted="muted" loop="loop">
-             <source src={video} type="video/mp4" />
-         </video>
+                    <video autoplay="autoplay" muted="muted" loop="loop">
+                        <source src={video} type="video/mp4" />
+                    </video>
              
-         <div class="container h-100">
-         <div class="d-flex h-100 text-center align-items-center">
-         <div class="w-100 text-white">
-         <h1 class="display-3">Welcome</h1>
+                    <div class="container h-100">
+                        <div class="d-flex h-100 text-center align-items-center">
+                            <div class="w-100 text-white">
+                                 <h1 class="display-3">Welcome</h1>
          
-         <Link to="/home"><button type="button" class="btn btn-dark">Start</button></Link>
-       </div>
+                                <Link to="/home"><button type="button" class="btn btn-dark">Start</button></Link>
+                             </div>
       
-     </div>
+                        </div>
      
-     </div>
+                    </div>
      
-     </div>
+                </div>
      
- </div>
- </header>
+            </div>
+        </header>
+    )
 }
 export default Landing;
