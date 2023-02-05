1f365807dd7c5c30793e34c7d85944e6ab24e3f4
Fran <franachard8@gmail>
2023-02-05 17:28:38 -0300

    Primera parte del login

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,23 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import Home from "./components/Home/Home";
 import Detail from "./components/Detail/Detail";
-import { Login } from "./components/Landing/Login";
-import { Profile } from "./components/Landing/Profile";
-import { Longout } from "./components/Landing/Logout";
-import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isAunthenticated } = useAuth0();
   return (
     <div className="App">
-      {isAunthenticated ? (
-        <>
-          <Profile />
-          <Longout />
-        </>
-      ) : (
-        <Login />
-      )}
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Landing} />
